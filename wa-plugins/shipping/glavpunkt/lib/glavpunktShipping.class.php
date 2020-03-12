9473799266cd21bd13d45678ee87659066563d3a
S0k0l0vMikhail <dolgopeh@gmail.com>
2020-03-12 14:04:55 +0300

    правки

--- wa-plugins/shipping/glavpunkt/lib/glavpunktShipping.class.php
+++ wa-plugins/shipping/glavpunkt/lib/glavpunktShipping.class.php
@@ -17,77 +17,11 @@ class glavpunktShipping extends waShipping
      */
     protected function calculate()
     {
-        $cost = 15; // заглушка, убрать когда будут реализованны все доставки
-        $deliveres = [];
         $cityTo = $this->getAddress('city');
         $activDeliveres = $this->optionsDelivery;
-        $tarifForPunktsInSelectedCity = $this->getPunktsWithTarif($cityTo, $this->cityFrom);
 
-        if (isset($cityTo)) {
-            if (isset($activDeliveres[1])) {
-                    $i = 0; //  счетчик, нужен для правильного формирования массива с пвз
-                foreach ($tarifForPunktsInSelectedCity as $k => $v) {
-
-                    $additional = isset($v["email"]) ? 'Email: ' . $v["email"] . '; ' : '';
-                    $additional .= isset($v["phone"]) ? 'Телефон: ' . $v["phone"] . '; ' : '';
-                    $additional .= isset($v["work_time"]) ? 'Режим работы: ' . $v["work_time"] . '; ' : '';
-
-                    $deliveres += [
-                        'variant_' . $i => [
-                            'name' => $v["address"], //название варианта доставки, например, “Наземный  транспорт”, “Авиа”, “Express Mail” и т. д.
-                            'comment' => 'описание необязательно', //необязательное описание варианта доставки
-                            'est_delivery' => $v["delivery_period"], //произвольная строка, содержащая  информацию о примерном времени доставки
-                            'currency' => $this->currency, //ISO3-код валюты, в которой рассчитана  стоимость  доставки
-                            'rate' => $v['tarif'], //точная стоимость доставки
-                            'type' => self::TYPE_PICKUP, //один из типов доставки waShipping::TYPE_TODOOR, waShipping::TYPE_PICKUP или waShipping::TYPE_POST
-                            'service' => $v["operator"] != "gp" ? $v['operator'] : 'Glavpunkt', //название службы доставки для указания компании, выполняющей фактическую доставку
-                            'custom_data' => [
-                                waShipping::TYPE_PICKUP => [
-                                    'id' => $v['id'],
-                                    'lat' => $v["geo_lat"],
-                                    'lng' => $v["geo_lng"],
-                                    'additional' => $additional,
-                                    'payment' => [
-                                        waShipping::PAYMENT_TYPE_CARD    => isset($v["card_accepted"]) ? true : false,
-                                        waShipping::PAYMENT_TYPE_CASH    => true,
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ];
-                    $i++;
-                }
-            };
-
-            if (isset($activDeliveres[2])){
-                $deliveres += [
-                    'variant_post' => array(
-                        'name' => 'Почта', //название варианта доставки, например, “Наземный  транспорт”, “Авиа”, “Express Mail” и т. д.
-                        'comment' => 'описание необязательно', //необязательное описание варианта доставки
-                        'est_delivery' => 'примерное время доставки', //произвольная строка, содержащая  информацию о примерном времени доставки
-                        'currency' => $this->currency, //ISO3-код валюты, в которой рассчитана  стоимость  доставки
-                        'rate' => $cost, //точная стоимость доставки
-                        'type' => self::TYPE_POST, //один из типов доставки waShipping::TYPE_TODOOR, waShipping::TYPE_PICKUP или waShipping::TYPE_POST
-                        'delivery_date' => date("Y-m-d H:i:s"), //дата доставки или интервал дат доставки в формате SQL DATETIME
-                        'service' => 'сервис', //название службы доставки для указания компании, выполняющей фактическую доставку
-                    ),
-                ];
-            };
-
-            if (isset($activDeliveres[3])){
-                $deliveres += [
-                    'variant_todoor' => array(
-                        'name' => 'Курьер', //название варианта доставки, например, “Наземный  транспорт”, “Авиа”, “Express Mail” и т. д.
-                        'comment' => 'описание необязательно', //необязательное описание варианта доставки
-                        'est_delivery' => 'примерное время доставки', //произвольная строка, содержащая  информацию о примерном времени доставки
-                        'currency' => $this->currency, //ISO3-код валюты, в которой рассчитана  стоимость  доставки
-                        'rate' => $cost, //точная стоимость доставки
-                        'type' => self::TYPE_TODOOR, //один из типов доставки waShipping::TYPE_TODOOR, waShipping::TYPE_PICKUP или waShipping::TYPE_POST
-                        'delivery_date' => date("Y-m-d H:i:s"), //дата доставки или интервал дат доставки в формате SQL DATETIME
-                        'service' => 'сервис', //название службы доставки для указания компании, выполняющей фактическую доставку
-                    ),
-                ];
-            };
+        if (isset($cityTo)){
+            $deliveres = $this->createArrayOfDeliveries($cityTo, $activDeliveres);
         } else {
             // обязательное сообщение ошибки для пользователя
             $deliveres = [
@@ -96,7 +30,6 @@ class glavpunktShipping extends waShipping
                     'comment' => 'Для расчета стоимости доставки укажите регион доставки',
                 ),
             ];
-
         }
 
         return $deliveres;
@@ -131,7 +64,7 @@ class glavpunktShipping extends waShipping
     /**
      * Возвращает массив со всеми пвз в городе доставки без цены
      *
-     * @param string $cityTo город куда доставка
+     * @param string $cityTo название города назначения
      * @return array
      * @throws Exception
      */
@@ -153,8 +86,8 @@ class glavpunktShipping extends waShipping
     /**
      * Возвращает массив со всеми пвз в городе доставки и ценой
      *
-     * @param string $cityTo город куда доставка
-     * @param string $cityFrom город отправки из настроек
+     * @param string $cityTo название города назначения
+     * @param string $cityFrom название город отправки из настроек
      * @return array
      * @throws Exception
      */
@@ -205,6 +138,88 @@ class glavpunktShipping extends waShipping
         }
 
         return $punkts;
+    }
+
+    private function createArrayOfDeliveries($cityTo, $activDeliveres) {
+
+        $cost = 15; // заглушка, убрать когда будут реализованны все доставки
+        $deliveres = [];
+        $currency = $this->currency;
+        $pickup = waShipping::TYPE_PICKUP;
+        $post = waShipping::TYPE_POST;
+        $todoor = waShipping::TYPE_TODOOR;
+        $cardPayment = waShipping::PAYMENT_TYPE_CARD;
+        $cashPayment = waShipping::PAYMENT_TYPE_CASH ;
+
+        $tarifForPunktsInSelectedCity = $this->getPunktsWithTarif($cityTo, $this->cityFrom);
+
+        if (isset($activDeliveres[1])) {
+            $i = 0; //  счетчик, нужен для правильного формирования массива с пвз
+            foreach ($tarifForPunktsInSelectedCity as $k => $v) {
+
+                $additional = isset($v["email"]) ? 'Email: ' . $v["email"] . '; ' : '';
+                $additional .= isset($v["phone"]) ? 'Телефон: ' . $v["phone"] . '; ' : '';
+                $additional .= isset($v["work_time"]) ? 'Режим работы: ' . $v["work_time"] . '; ' : '';
+
+                $deliveres += [
+                    'variant_' . $i => [
+                        'name' => $v["address"], //название варианта доставки, например, “Наземный  транспорт”, “Авиа”, “Express Mail” и т. д.
+                        'comment' => 'описание необязательно', //необязательное описание варианта доставки
+                        'est_delivery' => $v["delivery_period"], //произвольная строка, содержащая  информацию о примерном времени доставки
+                        'currency' => $currency, //ISO3-код валюты, в которой рассчитана  стоимость  доставки
+                        'rate' => $v['tarif'], //точная стоимость доставки
+                        'type' => $pickup, //один из типов доставки waShipping::TYPE_TODOOR, waShipping::TYPE_PICKUP или waShipping::TYPE_POST
+                        'service' => $v["operator"] != "gp" ? $v['operator'] : 'Glavpunkt', //название службы доставки для указания компании, выполняющей фактическую доставку
+                        'custom_data' => [
+                            $pickup => [
+                                'id' => $v['id'],
+                                'lat' => $v["geo_lat"],
+                                'lng' => $v["geo_lng"],
+                                'additional' => $additional,
+                                'payment' => [
+                                    $cardPayment    => isset($v["card_accepted"]) ? true : false,
+                                    $cashPayment    => true,
+                                ],
+                            ],
+                        ],
+                    ],
+                ];
+                $i++;
+            }
+        };
+
+        if (isset($activDeliveres[2])){
+            $deliveres += [
+                'variant_post' => array(
+                    'name' => 'Почта', //название варианта доставки, например, “Наземный  транспорт”, “Авиа”, “Express Mail” и т. д.
+                    'comment' => 'описание необязательно', //необязательное описание варианта доставки
+                    'est_delivery' => 'примерное время доставки', //произвольная строка, содержащая  информацию о примерном времени доставки
+                    'currency' => $currency, //ISO3-код валюты, в которой рассчитана  стоимость  доставки
+                    'rate' => $cost, //точная стоимость доставки
+                    'type' => $post, //один из типов доставки waShipping::TYPE_TODOOR, waShipping::TYPE_PICKUP или waShipping::TYPE_POST
+                    'delivery_date' => date("Y-m-d H:i:s"), //дата доставки или интервал дат доставки в формате SQL DATETIME
+                    'service' => 'сервис', //название службы доставки для указания компании, выполняющей фактическую доставку
+                ),
+            ];
+        };
+
+        if (isset($activDeliveres[3])){
+            $deliveres += [
+                'variant_todoor' => array(
+                    'name' => 'Курьер', //название варианта доставки, например, “Наземный  транспорт”, “Авиа”, “Express Mail” и т. д.
+                    'comment' => 'описание необязательно', //необязательное описание варианта доставки
+                    'est_delivery' => 'примерное время доставки', //произвольная строка, содержащая  информацию о примерном времени доставки
+                    'currency' => $currency, //ISO3-код валюты, в которой рассчитана  стоимость  доставки
+                    'rate' => $cost, //точная стоимость доставки
+                    'type' => $todoor, //один из типов доставки waShipping::TYPE_TODOOR, waShipping::TYPE_PICKUP или waShipping::TYPE_POST
+                    'delivery_date' => date("Y-m-d H:i:s"), //дата доставки или интервал дат доставки в формате SQL DATETIME
+                    'service' => 'сервис', //название службы доставки для указания компании, выполняющей фактическую доставку
+                ),
+            ];
+        };
+
+        return $deliveres;
+
     }
 
 }
